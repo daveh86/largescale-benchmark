4580554fe410e32f47eb24c23440a5bf5cb52b98
David Hows <howsdav@gmail.com>
2017-05-02 16:57:01 +1000

    Use the mongodb wirte_latency fields for latency calcs

--- stat-generate.py
+++ stat-generate.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 runtime = 0
 total_runtime = 3600
 filename = "results.csv"
-last_ops = {"insert": 0, "update": 0, "delete": 0, "query":0}
+last_ops = {"insert": 0, "update": 0, "delete": 0, "query":0, "writes":0, "write_latency":0}
 dbname = "POCDB"
 
 def get_last_ops(client):
@@ -14,15 +14,19 @@ def get_last_ops(client):
     ops = res['opcounters']
     gross = ops['insert'] + ops['update']
     last_gross = last_ops['insert'] + last_ops['update']
+    writes = ops['opLatencies']['writes']['ops'] - last_ops['writes']
+    write_latency = ops['opLatencies']['writes']['latency'] - last_ops['write_latency']
     last_ops['insert'] = ops['insert']
     last_ops['update'] = ops['update']
+    last_ops['writes'] = ops['opLatencies']['writes']['ops']
+    last_ops['write_latency'] = ops['opLatencies']['writes']['latency']
     collections = client[dbname].command('dbstats')['collections']
-    return ("%d,%d" % ((gross - last_gross),collections))
+    return ("%d,%d,%d,%d" % ((gross - last_gross),collections,writes,write_latency))
 
 
 client = MongoClient('mongodb://localhost:27017/')
 fhandle = open(filename, 'a')
-fhandle.write("time,relative_time,inserts,collections\n")
+fhandle.write("time,relative_time,inserts,collections,num_writes,write_latency\n")
 # Launch the app here?
 
 while (total_runtime > runtime):
